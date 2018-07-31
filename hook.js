276d17ebc95e6270e1b0806b67aa1186dc5d880e
Guilherme Pressutto <gpressutto5@gmail.com>
2018-07-31 19:07:10 -0300

    Updated extension to show airplane and fire reactions

--- hook.js
+++ hook.js
@@ -1,7 +1,8 @@
 const HOOK_MODULES = ['UFICentralUpdates', 'UFIFeedbackTargets'];
 const UPDATE_FEEDBACK_EVENT = 'update-feedback';
 const DEFAULT_SUPPORTED_REACTIONS_COUNT = 6;
-const REACTION_TYPE_ID_THANKFUL = 11;
+const REACTION_TYPE_ID_FIRE = 14;
+const REACTION_TYPE_ID_PLANE = 15;
 
 hookFacebookReactions();
 
@@ -18,7 +19,8 @@ function hookFacebookReactions() {
           var supportedReactions = feedbackObj.feedbacktarget.supportedreactions;
           // to prevent an infinite callback loop, only send an update if the Reactions haven't been modified yet
           if(supportedReactions.length === DEFAULT_SUPPORTED_REACTIONS_COUNT) {
-            supportedReactions.push(REACTION_TYPE_ID_THANKFUL); // inject the Thankful Reaction
+            supportedReactions.push(REACTION_TYPE_ID_FIRE); // inject the Thankful Reaction
+            supportedReactions.push(REACTION_TYPE_ID_PLANE); // inject the Thankful Reaction
             feedbackObj.feedbacktarget.supportedreactions = supportedReactions; // reassign the supportedreactions
             UFICentralUpdates.inform(UPDATE_FEEDBACK_EVENT, feedbackObj); // update the 'FeedbackTarget' ???? PROFIT!!
           }
